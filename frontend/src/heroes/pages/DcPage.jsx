41e6a5d3c01f00824f9664a3e721e07da61f93f8
mauricioCarcamo <mauriciorenecarcamorivera1@gmail.com>
2024-05-03 14:09:30 -0600

    estupides

---
+++ frontend/src/heroes/pages/DcPage.jsx
@@ -0,0 +1,17 @@
+import { HeroesList } from "../components"
+
+export const DcPage = () => {
+  return (
+    <>
+    <h1>
+      DC
+    </h1>
+    <hr />
+
+    <HeroesList publisher={"DC Comics"} />
+    
+    </>
+  )
+}
+
+
---
+++ frontend/src/heroes/pages/HeroPage.jsx
@@ -0,0 +1,51 @@
+import React, { useMemo } from 'react'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
+import { getHeroById } from '../';
+
+
+export const HeroPage = () => {
+  const { id } = useParams()
+
+  const hero = getHeroById( id )
+  // const hero = useMemo( () => { getHeroById( id ) }, [id]  ) 
+
+  const navigate = useNavigate()
+
+  const onNavigateBack = () => {
+    navigate(-1)
+  }
+
+  if (!hero) {
+    return <Navigate to="../" replace />
+  }
+
+  return (
+    <div className="row mt-5">
+      <div className="col-4 animate__animated animate__fadeInLeft">
+        <img 
+          src={`/assets/heroes/${ id }.jpg`} 
+          alt={ hero.superhero } 
+          className="img-thumbnail"
+        />
+      </div>
+      
+      <div className="col-8">
+        <h3>{ hero.superhero }</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego: </b> { hero.alter_ego } </li>
+          <li className="list-group-item"><b>Publisher: </b> { hero.publisher } </li>
+          <li className="list-group-item"><b>First appereance: </b> { hero.first_appearance } </li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{ hero.characters }</p>
+
+        <button 
+          className="btn btn-primary"
+          onClick={ onNavigateBack }>
+          Regresar
+        </button>
+      </div>
+    </div>
+  )
+}

1eadb4e46918b22ce9d9bad9a627b00a7a01d176
mauricioCarcamo <mauriciorenecarcamorivera1@gmail.com>
2024-05-03 14:18:05 -0600

    mover imagenes

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters, }) => {
-  const heroImageUrl = `/assets/heroes/${ id }.jpg`
+  const heroImageUrl = `heroes/${ id }.jpg`
   
   return (
     <div className="col animate__animated animate__fadeIn">
